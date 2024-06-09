fc502f81e03129530205c382a93ecec27259dce4
Nima Shoghi <nimashoghi@gmail.com>
2024-06-09 19:43:40 -0400

    Update

---
+++ frontier-cp-metadatas.sh
@@ -0,0 +1,10 @@
+mkdir -p ./metadatas
+
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/oc20/s2ef/s2ef/2M/train/metadata.npz" "./metadatas/oc20-2M-train.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/oc20/s2ef/s2ef/all/val_id/metadata.npz" "./metadatas/oc20-val_id.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/oc22/s2ef_total_train_val_test_lmdbs/data/oc22/s2ef-total/train/metadata.npz" "./metadatas/oc22-train.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/oc22/s2ef_total_train_val_test_lmdbs/data/oc22/s2ef-total/val_id/metadata.npz" "./metadatas/oc22-val_id.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/ani1x/lmdbs/train/ani1x_metadata.npz" "./metadatas/ani1x-train.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/ani1x/lmdbs/val/ani1x_val_metadata.npz" "./metadatas/ani1x-val.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/transition1x/lmdbs/train/metadata.npz" "./metadatas/transition1x-train.npz"
+cp "/lustre/orion/mat265/world-shared/nimashoghi/datasets/transition1x/lmdbs/train/metadata.npz" "./metadatas/transition1x-val.npz"
--- scripts/sync-all-jobs.py
+++ scripts/sync-all-jobs.py
@@ -1,17 +1,28 @@
 # %%
 from pathlib import Path
 
-base_path = Path(
-    "/lustre/orion/mat265/world-shared/nimashoghi/projectdata/jmppeft-realruns-final"
-)
-base_path
+base_paths = [
+    # Path(
+    #     "/lustre/orion/mat265/world-shared/nimashoghi/projectdata/jmppeft-realruns-final"
+    # ),
+    Path("/lustre/orion/mat265/world-shared/nimashoghi/repositories/jmp-peft/"),
+]
+base_paths
 
 # %%
-paths = list(base_path.glob("lltrainer/*/log/wandb/wandb/wandb/latest-run/"))
+paths = [
+    f
+    for base_path in base_paths
+    for f in base_path.glob("lltrainer/*/log/wandb/wandb/wandb/latest-run/")
+]
 paths
 
 # %%
 project = "jmp-proposal"
+commands = []
 for path in paths:
     command = f"wandb sync {path} --project {project} --include-synced"
-    print(command)
+    # print(command)
+    commands.append(command)
+
+print("; ".join(commands))
