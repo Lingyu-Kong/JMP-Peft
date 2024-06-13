b052603147ba46c10b447289c0238f5751f57de9
Nima Shoghi <nimashoghi@gmail.com>
2024-06-13 16:32:35 -0700

    Update

--- config/pretrain/graphormer-largest.py
+++ config/pretrain/graphormer-largest.py
@@ -40,10 +40,10 @@ def base_config_(config: M.PretrainConfig):
         betas=(0.9, 0.95),
         weight_decay=0.1,
     )
-    config.trainer.optimizer.log_grad_norm = True
+    config.trainer.optimizer.log_grad_norm = False
     config.trainer.optimizer.gradient_clipping = ll.GradientClippingConfig(
         value=2.0,
-        algorithm="norm",
+        algorithm="value",
     )
     # LR Scheduler settings
     config.lr_scheduler = M.LinearWarmupCosineAnnealingSchedulerConfig(
@@ -84,6 +84,10 @@ def graphormer_backbone_config_(
             backbone.graphormer_large_()
         case "xl":
             backbone.graphormer_extra_large_()
+
+            config.batch_size = 1
+            # backbone.embed_dim *= 2
+            backbone.layers = 128
         case _:
             raise ValueError(f"Unknown variant: {variant}")
     config.backbone = backbone.finalize()
@@ -111,7 +115,8 @@ def goc_backbone_config_(
 
 def fsdp_config_(config: M.PretrainConfig):
     config.fsdp = M.FSDPConfig(
-        gradient_checkpointing=False,
+        gradient_checkpointing=True,
+        cpu_offload=False,
     )
 
 
@@ -147,7 +152,7 @@ def no_metrics_config_(config: M.PretrainConfig):
 configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]] = []
 
 variants = ("base", "large", "xl")
-variants = ("base",)
+variants = ("xl",)
 backbone_config_fns = (graphormer_backbone_config_, goc_backbone_config_)
 backbone_config_fns = (graphormer_backbone_config_,)
 
@@ -180,7 +185,8 @@ for variant, backbone_config_ in itertools.product(variants, backbone_config_fns
         config.batch_size = 3
     elif variant == "xl":
         config.batch_size = 3
-    gradient_checkpointing_config_(config)
+    # gradient_checkpointing_config_(config)
+    fsdp_config_(config)
     no_metrics_config_(config)
 
     config.num_workers = 4
@@ -198,21 +204,6 @@ def run(config: M.PretrainConfig, model_cls: type[M.PretrainModel]):
     trainer.fit(model)
 
 
-def nsys_python_command_prefix(
-    configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]],
-) -> str:
-    assert len(configs) == 1, "Only one config is supported"
-    config, _ = configs[0]
-    profiler_directory = config.directory.resolve_subdirectory(config.id, "profiler")
-
-    return rf"nsys profile --wait=primary -w true -t cuda,nvtx,osrt,cudnn,cublas -s none -o {str(profiler_directory.absolute())}/nsight_report%q{{SLURM_PROCID}}%q{{SLURM_JOB_ID}}.qdrep -f true -x true "
-
-
-# %%
-
-runner = ll.Runner(run)
-runner.fast_dev_run(configs)
-
 # %%
 runner = ll.Runner(run)
 runner.fast_dev_run_session(
@@ -220,148 +211,5 @@ runner.fast_dev_run_session(
     n_batches=128,
     setup_commands=setup_commands,
     env=env,
-    python_command_prefix=nsys_python_command_prefix(configs),
+    pause_before_exit=True,
 )
-
-
-# %%
-from datetime import timedelta
-
-
-def submit_frontier(
-    runner: ll.Runner[M.PretrainConfig, None, type[M.PretrainModel]],
-    configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]],
-    debug: bool = False,
-    *,
-    nodes: int,
-    tasks_per_node: int = 8,
-    name: str | None = None,
-    print_command: bool = False,
-    setup_commands: list[str] = [],
-    env: dict[str, str] = {},
-    walltime: timedelta | None = None,
-    python_command_prefix: str | None = None,
-):
-    def compute_cpus_per_task(
-        configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]],
-    ):
-        # Max `num_workers` + 1 for the main process
-        max_num_workers = max(config.num_workers for config, _ in configs)
-        return max_num_workers + 1
-
-    def nodes_to_max_walltime(nodes: int) -> timedelta:
-        if 1 <= nodes <= 91:
-            return timedelta(hours=2.0)
-        elif 92 <= nodes <= 183:
-            return timedelta(hours=6.0)
-        else:
-            return timedelta(hours=12.0)
-
-    kwargs = {}
-    if name is not None:
-        kwargs["name"] = name
-    if debug:
-        kwargs["qos"] = "debug"
-
-    if walltime is None:
-        walltime = nodes_to_max_walltime(nodes)
-    return runner.submit_slurm(
-        configs,
-        snapshot=True,
-        account="mat265",
-        partition="batch",
-        nodes=nodes,
-        tasks_per_node=tasks_per_node,
-        cpus_per_task=compute_cpus_per_task(configs_copy),
-        walltime=walltime,
-        setup_commands=setup_commands,
-        env=env,
-        print_command=print_command,
-        python_command_prefix=python_command_prefix,
-        **kwargs,
-    )
-
-
-def submit_perlmutter(
-    runner: ll.Runner[M.PretrainConfig, None, type[M.PretrainModel]],
-    configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]],
-    debug: bool = False,
-    *,
-    nodes: int,
-    tasks_per_node: int = 4,
-    name: str | None = None,
-    print_command: bool = False,
-    setup_commands: list[str] = [],
-    env: dict[str, str] = {},
-    walltime: timedelta | None = None,
-    python_command_prefix: str | None = None,
-):
-    def compute_cpus_per_task(
-        configs: list[tuple[M.PretrainConfig, type[M.PretrainModel]]],
-    ):
-        # Max `num_workers` + 1 for the main process
-        max_num_workers = max(config.num_workers for config, _ in configs)
-        return max_num_workers + 1
-
-    kwargs = {}
-    if name is not None:
-        kwargs["name"] = name
-
-    if walltime is None:
-        walltime = timedelta(hours=24.0) if not debug else timedelta(hours=0.5)
-
-    return runner.submit_slurm(
-        configs,
-        snapshot=True,
-        account="m3641_g",
-        qos="preempt" if not debug else "debug_preempt",
-        constraint="gpu",
-        nodes=nodes,
-        tasks_per_node=tasks_per_node,
-        cpus_per_task=compute_cpus_per_task(configs_copy),
-        gpus_per_task=1,  # Perlmutter has 4 GPUs per node
-        walltime=walltime,
-        setup_commands=setup_commands,
-        env=env,
-        print_command=print_command,
-        python_command_prefix=python_command_prefix,
-        **kwargs,
-    )
-
-
-submit = submit_perlmutter if perlmutter else submit_frontier
-
-# nodes_list = [1, 8, 64, 128]
-# nodes_list = [128]
-# nodes_list = [1]
-# gpus_list = [4, 8, 64]
-gpus_list = [16, 32, 64]
-
-assert all(gpus % 4 == 0 for gpus in gpus_list)
-nodes_list = [gpus // 4 for gpus in gpus_list]
-
-commands: list[str] = []
-for nodes in nodes_list:
-    configs_copy: list[tuple[M.PretrainConfig, type[M.PretrainModel]]] = []
-    for config, model_cls in configs:
-        config_copy = config.clone()
-        config_copy.name_parts.append(f"nodes_{nodes}")
-        configs_copy.append((config_copy, model_cls))
-
-    runner = ll.Runner(run)
-    commands.append(
-        submit(
-            runner,
-            configs_copy,
-            nodes=nodes,
-            setup_commands=setup_commands,
-            env=env,
-            print_command=False,
-            name=f"jmp_n{nodes}",
-            # debug=True,
-            walltime=timedelta(hours=2.0),
-            python_command_prefix=nsys_python_command_prefix(configs_copy),
-        ).command
-    )
-
-print("; ".join(commands))
