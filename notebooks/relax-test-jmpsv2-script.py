c3b2d0fc47c0364a18b9b5a1d05f710eef29bb77
Nima Shoghi <nimashoghi@gmail.com>
2024-07-15 11:57:14 -0400

    Added JMP-L config

---
+++ notebooks/relax-test-jmpsv2-script.py
@@ -0,0 +1,268 @@
+# %%
+import os
+
+os.environ["LL_DISABLE_TYPECHECKING"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+
+import ll
+import rich
+import torch
+
+ll.pretty()
+
+torch.no_grad().__enter__()
+torch.inference_mode().__enter__()
+
+# %%
+from jmppeft.tasks.finetune.base import FinetuneMatBenchDiscoveryIS2REDatasetConfig
+
+dataset_config = FinetuneMatBenchDiscoveryIS2REDatasetConfig(
+    # sample_n=DatasetSampleNConfig(sample_n=16, seed=42)
+)
+print(dataset_config)
+
+dataset_og = dataset_config.create_dataset()
+dataset_og, len(dataset_og)
+
+# %%
+from pathlib import Path
+
+ckpt_path = Path(
+    "/net/csefiles/coc-fung-cluster/nima/shared/experiment-data/lltrainer/vye03sc4/checkpoint/last.ckpt"
+)
+# If the file is a symlink, get the target
+if ckpt_path.is_symlink():
+    print(f"Symlink found {ckpt_path} => {ckpt_path.resolve()}")
+    ckpt_path = ckpt_path.resolve()
+
+ckpt_path
+
+# %%
+from jmppeft.tasks.finetune import matbench_discovery as M
+
+default_dtype = torch.float32
+model = M.MatbenchDiscoveryModel.load_from_checkpoint(ckpt_path, map_location="cuda")
+model = model.to(default_dtype)
+model
+
+# %%
+from collections import Counter
+
+import jmppeft.modules.dataset.dataset_transform as DT
+import torch.utils._pytree as tree
+from lightning.fabric.utilities.apply_func import move_data_to_device
+from torch_geometric.data import Batch, Data
+
+
+def data_transform(data: Data):
+    data = model.data_transform(data)
+    data = Data.from_dict(
+        tree.tree_map(
+            lambda x: x.type(default_dtype)
+            if torch.is_tensor(x) and torch.is_floating_point(x)
+            else x,
+            data.to_dict(),
+        )
+    )
+    return data
+
+
+def composition(data: Batch):
+    return dict(Counter(data.atomic_numbers.tolist()))
+
+
+num_items = 1024
+
+dataset = DT.transform(dataset_og, data_transform)
+dataset = DT.sample_n_transform(dataset, n=num_items, seed=42)
+
+
+idx = 32
+data = Batch.from_data_list([dataset[idx]])
+rich.print(
+    data.to_dict(),
+    composition(data),
+)
+
+# %%
+from jmppeft.modules.relaxer import ModelOutput, Relaxer
+from matbench_discovery.energy import get_e_form_per_atom
+
+
+def model_fn(data, initial_data) -> ModelOutput:
+    model_out = model.forward_denormalized(data)
+
+    energy = model_out["y"]
+    forces = model_out["force"]
+    stress = model_out["stress"]
+
+    # JMP-S v2 energy is corrected_energy, i.e., DFT total energy
+    # This energy is now DFT total energy, we need to convert it to formation energy per atom
+    energy = get_e_form_per_atom(
+        {
+            "composition": composition(data),
+            "energy": energy,
+        }
+    )
+    assert isinstance(energy, torch.Tensor)
+
+    # Add the correction factor
+    if False:
+        energy += initial_data.y_formation_correction.item()
+
+    energy = energy.view(1)
+    forces = forces.view(-1, 3)
+    stress = stress.view(1, 3, 3) if stress.numel() == 9 else stress.view(1, 6)
+
+    return {
+        "energy": energy,
+        "forces": forces,
+        "stress": stress,
+    }
+
+
+data = move_data_to_device(data, model.device)
+model_fn(data, data)
+
+# %%
+from jmppeft.modules.relaxer import RelaxerConfig
+
+config = RelaxerConfig(
+    compute_stress=True,
+    stress_weight=1.0,
+    optimizer="FIRE",
+    fmax=0.05,
+    ase_filter="exp",
+)
+relaxer = Relaxer(
+    config=config,
+    model=model_fn,
+    collate_fn=model.collate_fn,
+    device=model.device,
+)
+rich.print(data.y_formation)
+relax_out = relaxer.relax(data)
+# rich.print(relax_out)
+
+energy = relax_out.atoms.get_total_energy()
+rich.print(energy, data.y_formation)
+
+# %%
+from collections import defaultdict
+
+import numpy as np
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+config = RelaxerConfig(
+    compute_stress=True,
+    stress_weight=1.0,
+    optimizer="FIRE",
+    # fmax=0.01,
+    # ase_filter="frechet",
+    fmax=0.05,
+    ase_filter="exp",
+)
+relaxer = Relaxer(
+    config=config,
+    model=model_fn,
+    collate_fn=model.collate_fn,
+    device=model.device,
+)
+
+dl = DataLoader(
+    dataset,
+    batch_size=1,
+    collate_fn=model.collate_fn,
+    shuffle=False,
+    num_workers=0,
+    pin_memory=True,
+)
+
+preds_targets = defaultdict[str, list[tuple[float, float]]](lambda: [])
+corrections = []
+
+for data in tqdm(dl, total=len(dl)):
+    data = move_data_to_device(data, model.device)
+    relax_out = relaxer.relax(data, verbose=False)
+
+    e_form_true = data.y_formation.item()
+    e_form_pred = relax_out.atoms.get_total_energy()
+    preds_targets["e_form"].append((e_form_pred, e_form_true))
+
+    e_above_hull_true = data.y_above_hull.item()
+    e_above_hull_pred = e_above_hull_true + (e_form_pred - e_form_true)
+    preds_targets["e_above_hull"].append((e_above_hull_pred, e_above_hull_true))
+
+    corrections.append(data.y_formation_correction.item())
+
+    nsteps = len(relax_out.trajectory.frames)
+
+    print(
+        f"# Steps: {nsteps}; e_form: P={e_form_pred:.4f}, GT={e_form_true:.4f}, Δ={abs(e_form_pred - e_form_true):.4f}"
+    )
+
+corrections = np.array(corrections)
+print(corrections.mean(), corrections.std())
+
+# %%
+import pickle
+
+base_save_dir = Path("/net/csefiles/coc-fung-cluster/nima/relaxation-dump-7-10/jmp-s2/")
+base_save_dir.mkdir(parents=True, exist_ok=True)
+
+with (base_save_dir / "preds_targets.pkl").open("wb") as f:
+    pickle.dump(dict(preds_targets), f)
+
+# %%
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matbench_discovery.metrics import stable_metrics
+
+sns.set_context("notebook")
+
+e_form_true, e_form_pred = zip(*preds_targets["e_form"])
+e_form_true = np.array(e_form_true)
+e_form_pred = np.array(e_form_pred)
+
+e_above_hull_true, e_above_hull_pred = zip(*preds_targets["e_above_hull"])
+e_above_hull_true = np.array(e_above_hull_true)
+e_above_hull_pred = np.array(e_above_hull_pred)
+
+rich.print(
+    {
+        "e_form": {"pred": e_form_pred.shape, "true": e_form_true.shape},
+        "e_above_hull": {
+            "pred": e_above_hull_pred.shape,
+            "true": e_above_hull_true.shape,
+        },
+    }
+)
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+axes = axes.flatten()
+
+ax = axes[0]
+sns.histplot(e_form_pred, label="Predicted formation energy", ax=ax)
+sns.histplot(e_form_true, label="True formation energy", ax=ax)
+ax.legend()
+
+ax = axes[1]
+sns.scatterplot(y=e_form_true, x=e_form_pred, ax=ax)
+ax.set_xlabel("Predicted formation energy")
+ax.set_ylabel("True formation energy")
+
+fig.tight_layout()
+plt.show()
+
+metrics = stable_metrics(e_above_hull_true, e_above_hull_pred, fillna=False)
+rich.print(metrics)
+print(metrics)
+
+import json
+
+with (base_save_dir / "metrics.json").open("w") as f:
+    json.dump(metrics, f, indent=2)
+# {'F1': 0.6994219653179191, 'DAF': 4.141176470588236, 'Precision': 0.711764705882353, 'Recall': 0.6875, 'Accuracy': 0.8984375, 'TPR': 0.6875, 'FPR': 0.05778301886792453, 'TNR': 0.9422169811320755, 'FNR': 0.3125, 'TP': 121, 'FP': 49, 'TN': 799, 'FN': 55, 'MAE': 0.05090716675272233, 'RMSE': 0.09208487361517845, 'R2': 0.8842208394346645}
+# %%
