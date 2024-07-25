27ac933d78a9fac411194e4a6eedec4cdad705c5
Nima Shoghi <nimashoghi@gmail.com>
2024-07-24 20:10:41 -0400

    Update

---
+++ tests/summit_test.py
@@ -0,0 +1,46 @@
+# %%
+import datetime
+import os
+import signal
+
+import nshrunner as nr
+
+
+def run_fn():
+    # Print current pid
+
+    def _handler(signum, frame):
+        print()
+        print()
+        print()
+        print(f"Received SIGU signal. signum: {signum}, frame: {frame}")
+        print()
+        print()
+        os.system("brequeue -h")
+        print()
+        print()
+
+    print("PID: ", os.getpid())
+
+    signal.signal(signal.SIGURG, _handler)
+
+    print("Press any key to exit.")
+    input()
+
+
+runner = nr.Runner(run_fn)
+runner.submit_lsf(
+    [()],
+    {
+        "summit": True,
+        "project": "MAT273",
+        "queue": "debug",
+        "nodes": 1,
+        "rs_per_node": 1,
+        "walltime": datetime.timedelta(hours=1),
+    },
+    snapshot=True,
+    env={
+        "LL_DISABLE_TYPECHECKING": "1",
+    },
+)
